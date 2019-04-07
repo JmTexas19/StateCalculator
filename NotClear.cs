c75336d4459fa427910a59a7ce8fbf42341715be
DazedFury <jmtexas19@gmail.com>
2019-04-07 00:49:13 -0400

    feat: Add state events

---
+++ NotClear.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator {
+    class NotClear : State {
+        public void nextState(int stateEvent) {
+
+        }
+    }
+}
